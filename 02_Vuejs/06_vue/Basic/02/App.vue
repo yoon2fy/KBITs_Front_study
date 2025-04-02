a605e393b984389cc0998a3d6e3b6fcf72c5a072
JiYoon Yang <yoon2fy@gmail.com>
2025-04-02 11:04:27 +0900

    06_vue(기본)
    06_vue(기본)

---
+++ 02_Vuejs/06_vue/Basic/02/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <hr>
+    <!-- 지역 컴포넌트로 등록하기 (예제 07-06) -->
+    <ul>
+      <CheckboxItem/>
+      <CheckboxItem/>
+      <CheckboxItem/>
+      <CheckboxItem/> 
+    </ul>
+    
+  </div>
+</template>
+
+
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+  export default {
+    name: 'App',
+    components: {CheckboxItem},
+  }
+  
+</script>
+
+
+
+<style></style>
+
+
+
