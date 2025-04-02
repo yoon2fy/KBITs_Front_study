a62b3fac011cc037fe3c85dd265589e4baf51a1f
JiYoon Yang <yoon2fy@gmail.com>
2025-04-02 11:10:47 +0900

    06_vue(심화)
    06_vue(심화)

---
+++ 02_Vuejs/06_vue/Deep/01/main.js
@@ -0,0 +1,7 @@
+// p.212 예제 07-27
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/main.css'
+
+createApp(App).mount('#app')
