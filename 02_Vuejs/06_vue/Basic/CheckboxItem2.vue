0246bda3bb56e4c65eb6ed5bdec535a31ec868d2
JiYoon Yang <yoon2fy@gmail.com>
2025-04-02 11:08:38 +0900

    06_vue(기본)
    06_vue(기본)

---
+++ 02_Vuejs/06_vue/Basic/CheckboxItem2.vue
@@ -0,0 +1,21 @@
+<!-- 예제 07-11 -->
+<template>
+    <li><input type="checkbox" :checked="idol.checked"/> {{ idol.name }} </li>
+</template>
+
+<script>
+    export default {
+        name: "CheckboxItem2",
+        props: {
+            id: [Number, String],
+            name: String,
+            checked: {
+                type: Boolean,
+                default: false,
+                required: false
+            }
+        }
+    }
+</script>
+
+
