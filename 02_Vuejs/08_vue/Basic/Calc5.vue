f2753c8451219e65cdba3e619b607b0e656b6360
JiYoon Yang <yoon2fy@gmail.com>
2025-04-03 13:51:56 +0900

    Update Calc5.vue

--- 02_Vuejs/08_vue/Basic/Calc5.vue
+++ 02_Vuejs/08_vue/Basic/Calc5.vue
@@ -6,16 +6,15 @@ o 이전 값과 새로운 값을 브라우저 콘솔에 출력함.
 <template>
     <div>
         X: <input type="text" v-model.number="state.x"><br/>
-        Y: <input type="text" v-model.number="state.y"><br/>
         <div>결과: {{ result }}</div>
     </div>
 </template>
 
 <script>
-import { reactive, computed, watch } from 'vue'
+import { watch, ref } from 'vue'
 
 export default {
-    name: "Calc4",
+    name: "Calc5",
     setup(){
         const x = ref(0);
         const result = ref(0)
