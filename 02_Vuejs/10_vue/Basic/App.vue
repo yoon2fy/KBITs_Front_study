8176e403fb4a6d51afb01e0e5ec477d12959883a
JiYoon Yang <yoon2fy@gmail.com>
2025-04-03 17:54:02 +0900

    10_vue(기본)
    10_vue(기본)

---
+++ 02_Vuejs/10_vue/Basic/App.vue
@@ -0,0 +1,19 @@
+<!-- 예제 11-01 -->
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestAPI = () => {
+  const url = "http://localhost:8000/todolist/gdhong";
+  axios.get(url).then((response) => {
+    console.log("# 응답객체 : ", response);
+  });
+}
+
+requestAPI();
+</script>
