b14f81652968a8236699a8d2d2f3bf3878619b3e
JiYoon Yang <yoon2fy@gmail.com>
2025-04-04 13:46:29 +0900

    07_vue(심화)

---
+++ 02_Vuejs/07_vue/Deep/v-model-test/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <InputName v-model:name="searchName" />
+    <h3>검색어 : {{ searchName }}</h3>
+  </div>
+</template>
+
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name: 'App',
+  components: { InputName },
+  data() {
+    return { searchName: 'John' };
+  }
+};
+</script>
+
+<!-- 
+
+o InputName.vue에서 name 프로퍼티를 받음.
+o v-model:name을 통해 searchName과 연결.
+o <input>의 value는 name과 연결되며,
+  @input 이벤트에서 $emit('update:name', 값)을 호출하여
+  부모 컴포넌트의 데이터를 변경.
+
+-->
