4aaa3677df9728123a8b3e1ea8d3b778c1241090
JiYoon Yang <yoon2fy@gmail.com>
2025-04-04 13:48:00 +0900

    07_vue(심화)
    07_vue(심화)

---
+++ 02_Vuejs/07_vue/Deep/provide-inject-test/SongList.vue
@@ -0,0 +1,23 @@
+<!-- 문제 (Page. 9)
+다음 조건을 만족하는 SongList 컴포넌트를 정의하세요.
+o App.vue에서 정의한 체크된 곡 수 값을 출력.
+-->
+
+<template>
+    <ul>
+        <SongListItem v-for="s in songs" :key="s.id" :song="s" />
+    </ul>
+    <div>체크된 곡 수 : {{ doneCount }}</div>
+</template>
+
+<script>
+import SongListItem from './SongListItem.vue';
+export default {
+    name: 'SongList',
+    components: {
+        SongListItem,
+    },
+    props: ['songs'],
+    inject: ['doneCount']
+};
+</script>
---
+++ 02_Vuejs/07_vue/Deep/provide-inject-test/SongListItem.vue
@@ -0,0 +1,19 @@
+<!-- 문제 (Page. 8)
+다음 조건을 만족하는 SongListItem 컴포넌트를 정의하세요.
+o song.done의 값에 따라 App.vue에서 정의한 icon를 설정. 
+-->
+
+<template>
+    <li>
+        <i :class="song.done ? icons.checked : icons.unchecked"></i>
+        {{ song.title }}
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'SongListItem',
+    inject: ['icons'],
+    props: ['song'],
+};
+</script>
